f4fe06a49862b61ed831e7ec86f9bb23f9c8135c
shayanmoosavi <moosavi.shayan@gmail.com>
2026-05-18 01:57:01 +0330

    added misc settings

---
+++ hyprland/.config/hypr/modules/settings/other.lua
@@ -0,0 +1,37 @@
+-- Other Settings
+-- ==============================================================================================================================================
+
+
+hl.config({
+    misc = {
+        disable_hyprland_logo = true
+    },
+
+    binds = {
+        allow_workspace_cycles = true,
+        workspace_back_and_forth = true
+    },
+
+    xwayland = {
+        force_zero_scaling = true
+    },
+
+    render = {
+        -- Auto; Attempts to reduce lag when there is only one fullscreen application on a screen (e.g. game).
+        direct_scanout = 2,
+        new_render_scheduling = true
+    },
+
+    cursor = {
+        warp_on_change_workspace = 1
+    },
+
+    -- For HDR monitors
+    --[[Some clients expect monitor to be in HDR mode prior to the client start. This breaks auto HDR activation and
+    can cause whitescreen and flickering. Use prefer_hdr to fix it]]
+    -- https://wiki.hypr.land/Configuring/Basics/Variables/#quirks
+    quirks = {
+        -- Gamescope only
+        prefer_hdr = 2
+    }
+})
