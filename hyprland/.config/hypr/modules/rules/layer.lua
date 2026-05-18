82074b04f7f28b68174332f454905db98143c59f
shayanmoosavi <moosavi.shayan@gmail.com>
2026-05-19 01:42:24 +0330

    added layer rules

---
+++ hyprland/.config/hypr/modules/rules/layer.lua
@@ -0,0 +1,8 @@
+-- Layer rules
+-- ==============================================================================================================================================
+
+-- Import helpers
+local helpers = require("hypr.modules.rules.helpers")
+
+helpers.Blur_rofi()
+helpers.Blur_notifications()
