f4e9a41b1a4e6ab9b4708e9f887331f3cc235580
shayanmoosavi <moosavi.shayan@gmail.com>
2026-05-19 18:38:29 +0330

    added description to keybinds

--- hyprland/.config/hypr/modules/keybinds/apps.lua
+++ hyprland/.config/hypr/modules/keybinds/apps.lua
@@ -4,20 +4,30 @@
 local default = require("hypr.modules.keybinds.defaults")
 
 -- Launch terminal
-hl.bind(default.MainMod .. " + " .. "Return", hl.dsp.exec_cmd(default.Terminal))
+hl.bind(default.MainMod .. " + " .. "Return", hl.dsp.exec_cmd(default.Terminal), {
+    description = "Launch terminal"
+})
 
 -- Launch Application Launcher
-hl.bind(default.MainMod .. " + " .. "Space", hl.dsp.exec_cmd("rofi -show drun"))
+hl.bind(default.MainMod .. " + " .. "Space", hl.dsp.exec_cmd("rofi -show drun"), {
+    description = "Launch Application Launcher"
+})
 
 -- Launch File Manager
-hl.bind(default.MainMod .. " + " .. "Shift + Return", hl.dsp.exec_cmd(default.FileManager))
+hl.bind(default.MainMod .. " + " .. "Shift + Return", hl.dsp.exec_cmd(default.FileManager), {
+    description = "Launch File Manager"
+})
 
 -- Launch Browser
-hl.bind(default.MainMod .. " + " .. "B", hl.dsp.exec_cmd(default.Browser))
+hl.bind(default.MainMod .. " + " .. "B", hl.dsp.exec_cmd(default.Browser), {
+    description = "Launch Browser"
+})
 
 -- Launch Clipboard Manager
 local clipse_cmd = default.Terminal .. "--class clipse -e clipse"
-hl.bind(default.MainMod .. " + " .. "V", hl.dsp.exec_cmd(clipse_cmd))
+hl.bind(default.MainMod .. " + " .. "V", hl.dsp.exec_cmd(clipse_cmd), {
+    description = "Launch Clipboard Manager"
+})
 
 -- # Change Wallpaper
 -- bind = $mainMod, W, exec, ~/.config/hypr/scripts/Wallpaper.sh
