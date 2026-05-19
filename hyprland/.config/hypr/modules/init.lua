6cf470aa4ed74740d0982ad6087674174d18276b
shayanmoosavi <moosavi.shayan@gmail.com>
2026-05-20 02:12:39 +0330

    added init.lua for the modules directory itself

---
+++ hyprland/.config/hypr/modules/init.lua
@@ -0,0 +1,8 @@
+local modules = "hypr.modules"
+
+require(modules .. ".autostart")
+require(modules .. ".env")
+require(modules .. ".monitors")
+require(modules .. ".keybinds")
+require(modules .. ".rules")
+require(modules .. ".settings")
