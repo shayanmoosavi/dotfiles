21ac628117a424da525057f5efd1779ca560028a
shayanmoosavi <moosavi.shayan@gmail.com>
2026-05-19 17:35:05 +0330

    added monitor config

---
+++ hyprland/.config/hypr/modules/monitors.lua
@@ -0,0 +1,15 @@
+-- Monitors
+-- ==============================================================================================================================================
+
+-- The default recommended configuration that works for most use cases
+hl.monitor({
+    output = "",
+    mode = "preferred",
+    position = "auto",
+    scale = "1",
+})
+
+--[[
+The default configuration will suffice for most use cases. For more advanced configurations such as dual monitors, see the Hyprland documentation.
+https://wiki.hypr.land/Configuring/Basics/Monitors/
+]]
