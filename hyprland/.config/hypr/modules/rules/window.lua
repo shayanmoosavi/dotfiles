e4874a62095acff79bd61f435db4c87cf088d1ec
shayanmoosavi <moosavi.shayan@gmail.com>
2026-05-23 23:14:48 +0330

    simplified window.lua further by adding apply_rules

--- hyprland/.config/hypr/modules/rules/window.lua
+++ hyprland/.config/hypr/modules/rules/window.lua
@@ -4,29 +4,8 @@
 -- Import helpers
 local tags = require("utils.rules.tags")
 
--- Tags
--- --------------------------------------------------------------------------------------------------------------------------------------
--- Tagging certain grouped windows for easier window rules
-
-tags.tag_browser_type()
-tags.tag_games()
-tags.tag_settings()
-
--- Window rule specs for tagging different window categories
-local tag_specs = require("modules.rules.declarative").Tags
-
--- Apply window rules
-for _, spec in pairs(tag_specs) do
-    hl.window_rule(spec)
-end
-
--- Window rules
--- --------------------------------------------------------------------------------------------------------------------------------------
-
 -- Helper function for floating and centering windows
 local function float_and_center(specs)
-    local specs = specs or {}
-    specs.name = specs.name or "float-center"
     local float_and_center_specs = {
         float = true,
         center = true
@@ -39,37 +18,61 @@ local function float_and_center(specs)
     hl.window_rule(float_and_center_specs)
 end
 
+-- Helper function for applying window rules
+local function apply_rules(specs, is_float_center)
+    local is_float_center = is_float_center or false
+
+    if is_float_center then
+        for _, spec in pairs(specs) do
+            float_and_center(spec)
+        end
+    else
+        for _, spec in pairs(specs) do
+            hl.window_rule(spec)
+        end
+    end
+end
+
+-- Tags
+-- --------------------------------------------------------------------------------------------------------------------------------------
+-- Tagging certain grouped windows for easier window rules
+
+tags.tag_browser_type()
+tags.tag_games()
+tags.tag_settings()
+
+-- Window rule specs for tagging different window categories
+local tag_specs = require("modules.rules.declarative").Tags
+
+-- Apply window rules
+apply_rules(tag_specs)
+
+-- Window rules
+-- --------------------------------------------------------------------------------------------------------------------------------------
+
 -- Window rule specs for no blur
 local no_blur_specs = require("modules.rules.declarative").NoBlur
 
 -- Apply window rules
-for _, spec in pairs(no_blur_specs) do
-    hl.window_rule(spec)
-end
+apply_rules(no_blur_specs)
 
 -- Window rule specs for no idle
 local no_idle_specs = require("modules.rules.declarative").NoIdle
 
 -- Apply no idle window rule specs
-for _, spec in pairs(no_idle_specs) do
-    hl.window_rule(spec)
-end
+apply_rules(no_idle_specs)
 
 -- Window rule specs for floating and centering windows
 local float_center_specs = require("modules.rules.declarative").FloatCenter
 
 -- Apply float center window rule specs
-for _, specs in pairs(float_center_specs) do
-    float_and_center(specs)
-end
+apply_rules(float_center_specs, true)
 
 -- Window rule specs for picture-in-picture
 local pip_specs = require("modules.rules.declarative").PiP
 
 -- Apply picture-in-picture window rule specs
-for _, spec in pairs(pip_specs) do
-    hl.window_rule(spec)
-end
+apply_rules(pip_specs)
 
 -- Move to workspace window rules
 local workspace_categories = {
