90c4b533755376d27d546460f72d800e7b638c2f
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-01 19:05:12 +0330

    removed the redundant modules

--- hyprland/.config/hypr/modules/keybinds/apps.lua
+++
@@ -1,9 +0,0 @@
--- Application Keybinds
--- ==============================================================================================================================================
-
--- Importing the declarative keybind specs and the keybind manager
-local app_keybinds = require("modules.keybinds.declarative.specs").apps
-local apply_keybinds = require("utils.keybinds.manager").apply_keybinds
-
--- Applying the keybinds using the manager
-apply_keybinds(app_keybinds)
--- hyprland/.config/hypr/modules/keybinds/special.lua
+++
@@ -1,9 +0,0 @@
--- Special Keys Keybinds
--- ==============================================================================================================================================
-
--- Importing the declarative keybind specs and the keybind manager
-local special_keybinds = require("modules.keybinds.declarative.specs").special
-local apply_keybinds = require("utils.keybinds.manager").apply_keybinds
-
--- Applying the keybinds using the manager
-apply_keybinds(special_keybinds)
