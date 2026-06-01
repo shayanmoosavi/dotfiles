e820e379c7ee8a9234a9129883b472878336dc45
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-01 18:59:24 +0330

    simplified bind logic

--- hyprland/.config/hypr/utils/keybinds/manager.lua
+++ hyprland/.config/hypr/utils/keybinds/manager.lua
@@ -13,10 +13,10 @@ local function bind(specs)
     local desc = specs.description
 
     -- Get the options and arguments from the declarative specs
-    local opts = specs.opts or nil
-    local args = opts and opts.args or nil
-    local repeating = opts and opts.repeating or nil
-
+    local opts = specs.opts or {}
+    local args = opts.args
+    local repeating = opts.repeating or false
+    local mouse = opts.mouse or false
 
     local dsp = dispatcher[dsp_str]
 
@@ -24,29 +24,16 @@ local function bind(specs)
         error("Invalid dispatcher: " .. tostring(dsp_str))
     end
 
+    local bind_options = {
+        description = desc,
+        repeating = repeating,
+        mouse = mouse,
+    }
     -- If there are no arguments, call the dispatcher directly. Otherwise, call it with the provided arguments.
     if not args then
-        if repeating then
-            hl.bind(key, dsp, {
-                description = desc,
-                repeating = true,
-            })
-        else
-            hl.bind(key, dsp, {
-                description = desc,
-            })
-        end
+        hl.bind(key, dsp, bind_options)
     else
-        if repeating then
-            hl.bind(key, dsp(args), {
-                description = desc,
-                repeating = true,
-            })
-        else
-            hl.bind(key, dsp(args), {
-                description = desc,
-            })
-        end
+        hl.bind(key, dsp(args), bind_options)
     end
 end
 
