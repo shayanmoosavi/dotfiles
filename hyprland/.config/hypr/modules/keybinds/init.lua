a366c270c2537f54783c0e13a85de845af3293ff
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-01 19:03:42 +0330

    preparing for the transition to the declarative approach

--- hyprland/.config/hypr/modules/keybinds/init.lua
+++ hyprland/.config/hypr/modules/keybinds/init.lua
@@ -1,4 +1,15 @@
-require("modules.keybinds.apps")
+local specs = require("modules.keybinds.declarative.specs")
+local apply_keybinds = require("utils.keybinds.manager").apply_keybinds
+local exporter = require("utils.keybinds.exporter")
+
+-- Apply the keybinds
+for _, keybind_category in pairs(specs) do
+    apply_keybinds(keybind_category)
+end
+
 require("modules.keybinds.hyprbinds")
-require("modules.keybinds.special")
 require("modules.keybinds.workspaces")
+
+-- Dump the fully evaluated table to a file for keybinds_reference.py
+local json_path = os.getenv("HOME") .. "/.config/hypr/scripts/resources/keybinds.json"
+exporter.export(specs, json_path)
