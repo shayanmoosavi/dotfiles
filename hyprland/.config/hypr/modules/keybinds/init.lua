d93b936ad3760dad6b727422d6aad3989c907c8c
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-02 03:59:42 +0330

    updated init.lua to reflect changes

--- hyprland/.config/hypr/modules/keybinds/init.lua
+++ hyprland/.config/hypr/modules/keybinds/init.lua
@@ -1,13 +1,7 @@
 local specs = require("modules.keybinds.declarative.specs")
-local apply_keybinds = require("utils.keybinds.manager").apply_keybinds
 local exporter = require("utils.keybinds.exporter")
 
--- Apply the keybinds
-for _, keybind_category in ipairs(specs) do
-    apply_keybinds(keybind_category)
-end
-
-require("modules.keybinds.workspaces")
+require("modules.keybinds.bindall")
 
 -- Dump the fully evaluated table to a file for keybinds_reference.py
 local json_path = os.getenv("HOME") .. "/.config/hypr/scripts/resources/keybinds.json"
