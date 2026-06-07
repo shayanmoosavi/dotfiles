d7380abb0d24876faf728b8feeff2de1c3e0ef88
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-07 22:23:13 +0330

    restructured animations declaration and removed the helper

--- hyprland/.config/hypr/modules/animations/declarative/animations.lua
+++ hyprland/.config/hypr/modules/animations/declarative/animations.lua
@@ -1,36 +1,30 @@
 -- Declarative specification for setting animation curves
 -- ==============================================================================================================================================
 
--- Importing curve definitions
-local curves = require("modules.animations.declarative.curves")
-
--- Importing helper functions
-local get_curve = require("modules.animations.declarative.animations").get_curve
-
--- Declaring specs
--- --------------------------------------------------------------------------------------------------------------------------------------
-
 local animations = {
     -- Setting animation curves
     {
         leaf = "windows",
         enabled = true,
         speed = 6,
-        spring = get_curve(curves, "windows_curve"),
+        name = "windows_curve",
+        curve_type = "spring",
         style = "popin"
     },
     {
         leaf = "workspaces",
         enabled = true,
         speed = 6,
-        spring = get_curve(curves, "workspace_curve"),
+        name = "workspace_curve",
+        curve_type = "spring",
         style = "slidefade 70%"
     },
     {
         leaf = "layers",
         enabled = true,
         speed = 4,
-        spring = get_curve(curves, "layer_curve"),
+        name = "layer_curve",
+        curve_type = "spring",
         style = "slide right"
     }
 }
