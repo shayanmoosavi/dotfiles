2aa1a5c3d57781935bdec67e4587f0483b06a42d
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-07 03:39:20 +0330

    removed old modules

--- hyprland/.config/hypr/modules/settings/other.lua
+++
@@ -1,8 +0,0 @@
--- Other Settings
--- ==============================================================================================================================================
-
--- Importing declarative specs
-local decl_settings = "modules.settings.declarative"
-local other = require(decl_settings .. ".other")
-
-hl.config(other)
