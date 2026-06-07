e524a57c8ae38640f4765d95c65755ccd56e1447
shayanmoosavi <moosavi.shayan@gmail.com>
2026-06-07 21:10:35 +0330

    restructured curve specs declaration

--- hyprland/.config/hypr/modules/animations/declarative/curves.lua
+++ hyprland/.config/hypr/modules/animations/declarative/curves.lua
@@ -3,32 +3,23 @@
 
 local curves = {
     -- Spring curve definitions
-    {
-        name = "windows_curve",
-        specs = {
-            type = "spring",
-            mass = 1,
-            stiffness = 40,
-            dampening = 8
-        }
+    windows_curve = {
+        type = "spring",
+        mass = 1,
+        stiffness = 40,
+        dampening = 8
     },
-    {
-        name = "workspace_curve",
-        specs = {
-            type = "spring",
-            mass = 1,
-            stiffness = 30,
-            dampening = 8,
-        }
+    workspace_curve = {
+        type = "spring",
+        mass = 1,
+        stiffness = 30,
+        dampening = 8
     },
-    {
-        name = "layer_curve",
-        specs = {
-            type = "spring",
-            mass = 1,
-            stiffness = 80,
-            dampening = 12,
-        }
+    layer_curve = {
+        type = "spring",
+        mass = 1,
+        stiffness = 80,
+        dampening = 12
     }
 }
 
